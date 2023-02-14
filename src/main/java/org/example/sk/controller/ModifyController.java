93026cf0b221f30d10d75d58f2cd08ec83e0f5e8
Eldar <9eldik7@gmail.com>
2023-02-14 12:47:20 +0300

    controller take only "current"

--- src/main/java/org/example/sk/controller/ModifyController.java
+++ src/main/java/org/example/sk/controller/ModifyController.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
 import org.example.sk.model.request.ModifyRequest;
-import org.example.sk.model.responce.ModifyResponse;
 import org.example.sk.service.ExampleTableService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,14 +18,8 @@ public class ModifyController {
 
     @PostMapping("/modify")
     public ResponseEntity<JsonNode> modify(@RequestBody ModifyRequest request) {
-        ModifyResponse modifyResponse = exampleTableService.modify(request.id(), request.add());
-
-        if (modifyResponse.getStatusCode() == -1) {
-            return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
-        } else {
-            ObjectNode response = JsonNodeFactory.instance.objectNode();
-            response.put("current", modifyResponse.getCurrent());
-            return ResponseEntity.ok(response);
-        }
+        ObjectNode response = JsonNodeFactory.instance.objectNode();
+        response.put("current", exampleTableService.modify(request.id(), request.add()));
+        return ResponseEntity.ok(response);
     }
 }
