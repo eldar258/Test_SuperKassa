8fa8b57c1cb9ebdb3ee29abc0cef9bf89f4faba9
Eldar <9eldik7@gmail.com>
2023-02-14 15:12:14 +0300

    simplified service and repository

--- src/main/java/org/example/sk/repository/ExampleTableRepository.java
+++ src/main/java/org/example/sk/repository/ExampleTableRepository.java
@@ -1,19 +1,10 @@
 package org.example.sk.repository;
 
-import java.util.Optional;
 import org.example.sk.model.entity.ExampleTable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExampleTableRepository extends JpaRepository<ExampleTable, Long> {
 
-    Optional<ExampleTable> findById(long id);
-
-    @Modifying
-    @Query(value = "UPDATE sk_example_table SET obj = jsonb_set(obj, '{current}', CAST(CAST((CAST((obj->>'current') AS integer) + CAST(:add AS integer)) AS text) AS jsonb), false) WHERE id = :id", nativeQuery = true)
-    int updateCurrent(@Param("id") Long id, @Param("add") int add);
 }
--- src/main/java/org/example/sk/service/ExampleTableService.java
+++ src/main/java/org/example/sk/service/ExampleTableService.java
@@ -1,7 +1,5 @@
 package org.example.sk.service;
 
-import java.sql.SQLException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.example.sk.exception.DataUpdateException;
 import org.example.sk.model.entity.ExampleTable;
@@ -17,21 +15,13 @@ public class ExampleTableService {
 
     @Transactional
     public int modify(long id, int add) throws Exception {
-        int updateCount;
-        try {
-            updateCount = exampleTableRepository.updateCurrent(id, add);
-        } catch (RuntimeException sqlException) {
-            sqlException.printStackTrace();
-            throw new SQLException(sqlException.getMessage());
-        }
+        ExampleTable exampleTable = exampleTableRepository.findById(id).orElseThrow(
+                () -> new DataUpdateException("Modify not found by id " + id)
+        );
 
-        if (updateCount == 0) throw new DataUpdateException("Update count == 0");
-
-        int result = 0;
-        Optional<ExampleTable> exampleTableOptional = exampleTableRepository.findById(id);
-        if (exampleTableOptional.isPresent()) {
-            result = exampleTableOptional.get().getObj().getCurrent();
-        }
+        int result = exampleTable.getObj().getCurrent() + add;
+        exampleTable.getObj().setCurrent(result);
+        exampleTableRepository.save(exampleTable);
 
         return result;
     }
